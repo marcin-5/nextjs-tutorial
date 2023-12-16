f872cc4a310883657549c96d5010b325948fac9f
Marcin Bojara <marcin_5@mail.ru>
2023-12-16 17:59:55 +0100

    fix get all tasks

--- components/TaskList.jsx
+++ components/TaskList.jsx
@@ -4,7 +4,7 @@ import { getAllTasks } from '@/utils/actions';
 import DeleteForm from './DeleteForm';
 
 const TaskList = async () => {
-  const tasks = await getAllTasks;
+  const tasks = await getAllTasks();
 
   if (tasks.length === 0)
     return <h2 className='mt-8 font-medium text-lg'>No tasks to show...</h2>;
