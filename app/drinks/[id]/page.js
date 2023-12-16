0f3159312ab6c32ba49467f8185ec74743f5d2b8
Marcin Bojara <marcin_5@mail.ru>
2023-12-16 14:31:20 +0100

    add display images

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
@@ -19,6 +20,14 @@ const SingleDrink = async ({ params }) => {
       <Link href='/drinks' className='btn btn-primary mt-8 mb-12'>
         back to drinks
       </Link>
+      <Image
+        src={imgSrc}
+        width={300}
+        height={300}
+        className='w-48 h-48 rounded-lg shadow-lg mb-4'
+        priority
+        alt={title}
+      />
       <h1 className='text-4xl mb-8'>{title}</h1>
     </div>
   );
