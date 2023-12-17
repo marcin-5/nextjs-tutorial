bc33d520d14b92c109a11c685e250c5055f35166
Marcin Bojara <marcin_5@mail.ru>
2023-12-17 01:58:24 +0100

    add edit task

--- utils/actions.js
+++ utils/actions.js
@@ -1,6 +1,7 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 import prisma from '@/utils/db';
 
@@ -30,4 +31,31 @@ export const deleteTask = async (formData) => {
   await prisma.task.delete({ where: { id } });
 
   revalidatePath('/tasks');
+};
+
+export const getTask = async (id) => {
+  return prisma.task.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
+export const editTask = async (formData) => {
+  const id = formData.get('id');
+  const content = formData.get('content');
+  const completed = formData.get('completed');
+
+  await prisma.task.update({
+    where: {
+      id: id,
+    },
+    data: {
+      content: content,
+      completed: completed === 'on',
+    },
+  });
+  // redirect won't works unless the component has 'use client'
+  // another option, setup the editTask in the component directly
+  redirect('/tasks');
 };
